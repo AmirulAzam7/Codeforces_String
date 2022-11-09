f008a13f71109e6c371bb0fdeba7094a7f50f014
Amirul Azam <109798823+AmirulAzam7@users.noreply.github.com>
2022-11-09 22:55:37 +0600

    Create sheet4_A.c

---
+++ sheet4_A.c
@@ -0,0 +1,19 @@
+    #include<stdio.h>
+    #include<string.h>
+     
+    int main ()
+    {
+        char s1[1000],s2[1000],s3[1000];
+     
+        scanf("%s %s",s1,s2);
+     
+        printf("%d %d\n",strlen(s1),strlen(s2));
+     
+        printf("%s %s",s1,s2);
+     
+     
+     
+     
+        return 0;
+    }
+
