0d11eb977ba4677480f27a5266f07e845f08f758
Amirul Azam <109798823+AmirulAzam7@users.noreply.github.com>
2022-11-09 23:09:07 +0600

    Create sheet4_U.cpp

---
+++ sheet4_U.cpp
@@ -0,0 +1,49 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+     
+    int main()
+    {
+       string s;
+       cin>>s;
+     
+       int e=0,g=0,y=0,p=0,t=0;
+     
+       for(int i=0; i<s.size(); i++)
+       {
+           if(s[i]=='e' || s[i]=='E')
+           {
+               e++;
+           }
+           else if(s[i]=='g' || s[i]=='G')
+           {
+               g++;
+           }
+           else if(s[i]=='y' || s[i]=='Y')
+           {
+               y++;
+           }
+           else if(s[i]=='p' || s[i]=='P')
+           {
+               p++;
+           }
+           else if(s[i]=='t' || s[i]=='T')
+           {
+               t++;
+           }
+       }
+     
+       int arr[5]={e,g,y,p,t};
+     
+       int minn = arr[0];
+     for(int j=1; j<5; j++)
+     {
+       if(arr[j]<minn)
+       {
+           minn = arr[j];
+       }
+     }
+     cout<<minn<<endl;
+     
+     
+        return 0;
+    }
